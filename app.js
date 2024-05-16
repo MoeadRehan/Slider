adf692ed0f05bbc460143c0e25ba4793e98984c9
Moead_Rehan <moeadrehan124125@gmail.com>
2024-05-16 18:36:56 +0500

    Second Commit

--- app.js
+++ app.js
@@ -9,22 +9,34 @@ slides.forEach(function(slide, index){
 let counter = 0;
 nextBtn.addEventListener('click', function(){
     counter++;
-    if(counter === slides.length){
-        counter = 0;
-    }
+    // if(counter === slides.length){
+    //     counter = 0;
+    // }
     scroll();
 })
 
 prevBtn.addEventListener("click", function(){
     counter--;
-    if(counter < 0){
-        counter = slides.length-1;
-    }
+    // if(counter < 0){
+    //     counter = slides.length-1;
+    // }
     scroll();
 });
 
 function scroll(){
+    if(counter < slides.length-1){
+        nextBtn.style.display = "block";
+    }else{
+        nextBtn.style.display = "none";
+    }
+
+    if(counter > 0){
+        prevBtn.style.display = "block";
+    }else{
+        prevBtn.style.display = "none"
+    }
     slides.forEach(function(slide){
         slide.style.transform = `translateX(-${counter * 100}%)`;
     });
 }
+prevBtn.style.display = "none";
